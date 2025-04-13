2d69c662310fa7671d404531ab7cf19df4dc70e9
md-shafi-ur-rehaman <mohammadshafiq.rahaman@gmail.com>
2025-04-14 00:19:56 +0530

    revese an array

---
+++ Arrays/Reverse-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+      void reverseArray(vector<int> &arr) {
+          // code here
+          int l = 0;
+          int r = arr.size() -1;
+          
+          while(l<r){
+              swap(arr[l++],arr[r--]);
+          }
+      }
+};
